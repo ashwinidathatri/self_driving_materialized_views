1ffb7be97911d5e63562d63bebf29438a85ae9e2
Richhiey Thomas <richhiey.thomas@gmail.com>
2019-06-08 23:06:33 +0200

    Changes for OpenAI environment

--- project/env/database_env/database_env/envs/database_env.py
+++ project/env/database_env/database_env/envs/database_env.py
@@ -14,6 +14,9 @@ class DatabaseEnv(gym.Env):
         # Number of tables in the database being considered
         N_TABLES = args['n_tables']
 
+        # Maximum number of steps in an episode
+        N_MAX_STEPS = args['max_steps']
+
         # Define action and observation space
         # They must be gym.spaces objects
         self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
@@ -21,17 +24,50 @@ class DatabaseEnv(gym.Env):
             low = 0,
             high = 1,
             shape = (
-                N_TABLES,
                 N_TABLES
             ),
             dtype = np.uint8
         )
 
+        # Capture information about episode to replay the same
+        # on the real database
+        self.history = { }
+        self.current_step = 0
+        self.max_steps = N_MAX_STEPS
+        self.current_views = []
+
+
     def step(self, action):
-        pass
- 
+        # Use the action predicted by agent to modify the
+        # database environment
+        self._take_action(action)
+
+        # See the state of the new environment
+        obs = self._next_observation(action)
+
+        # Calculate reward of the action
+        delay_modifier = (self.current_step / MAX_STEPS)
+        reward = self.candidate_cost * delay_modifier
+
+        # Log some info about this training step
+        info = {}
+        self.current_step += 1
+        # Check whether we have reached end of episode
+        done = self.current_step < self.max_steps
+
+        return obs, reward, done, info
+
+    # Reset the state of the environment to an initial state
     def reset(self):
-        pass
- 
+        self.candidate_cost = INITIAL_ACCOUNT_BALANCE
+        return self._next_observation()
+
     def render(self, mode='human', close=False):
         pass
+
+    def _next_observation(self, params):
+        pass
+
+    def _take_action(self, action):
+        pass
+        
--- project/env/env_runner.py
+++ project/env/env_runner.py
@@ -1,1 +1,4 @@
 import numpy as np
+
+class EnvRunner():
+	pass

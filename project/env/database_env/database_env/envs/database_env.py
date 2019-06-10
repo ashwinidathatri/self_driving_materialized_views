4553b4e0ff43e071b4ebb75be9da9699a9dae06a
Richhiey Thomas <richhiey.thomas@gmail.com>
2019-06-10 18:57:18 +0200

    Final changes for dummy env

--- project/env/database_env/database_env/envs/database_env.py
+++ project/env/database_env/database_env/envs/database_env.py
@@ -3,6 +3,10 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
 import math
+import random
+
+CREATE_VIEW_ACTION = 1
+N_JOIN_COMBINATIONS = 15
 
 class DatabaseEnv(gym.Env):  
 	metadata = {'render.modes': ['human']}   
@@ -15,10 +19,14 @@ class DatabaseEnv(gym.Env):
 		
 		# Number of tables in the database being considered
 		N_TABLES = args['n_tables']
+		# N_JOIN_COMBINATIONS = 
+		# 		int(math.factorial(N_TABLES) / 
+		# 			math.factorial(N_TABLES - N_MAX_JOINS) * math.factorial(N_MAX_JOINS))
 
 		# Maximum number of steps in an episode
 		N_MAX_STEPS = args['max_steps']
 		N_MAX_JOINS = 2
+
 		# Define action and observation space
 		# They must be gym.spaces objects
 		self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
@@ -26,8 +34,7 @@ class DatabaseEnv(gym.Env):
 			low = 0,
 			high = 1,
 			shape = (
-				1,
-				int(math.factorial(N_TABLES) / math.factorial(N_TABLES - N_MAX_JOINS) * math.factorial(N_MAX_JOINS))
+				N_JOIN_COMBINATIONS,
 			),
 			dtype = np.uint8
 		)
@@ -39,57 +46,113 @@ class DatabaseEnv(gym.Env):
 		self.max_steps = N_MAX_STEPS
 		self.current_views = []
 		self.candidate_cost = 100
+		self._obs_space = np.zeros(N_JOIN_COMBINATIONS)
+		self._current_action = np.zeros(N_JOIN_COMBINATIONS)
+		self._lru_info = np.zeros(N_JOIN_COMBINATIONS)
 
-
-	def step(self, action):
+	def step(self, action, candidate):
 		# Use the action predicted by agent to modify the
 		# database environment and calculate reward of the action
 		delay_modifier = (self.current_step / self.max_steps)
-		reward = self._take_action(action, delay_modifier)
-
+		# print(self._obs_space)
+		reward = self._take_action(action, candidate, delay_modifier)
+		# print(self._obs_space)
 		# See the state of the new environment
-		obs = self._next_observation(action)
+		obs = self._next_observation()
 
 		# Log some info about this training step
 		info = {}
 		self.current_step += 1
 
 		# Check whether we have reached end of episode
-		done = self.current_step < self.max_steps
+		done = self.current_step >= self.max_steps
 
 		return obs, reward, done, info
 
 	# Reset the state of the environment to an initial state
 	def reset(self):
+		self.history = { }
+		self.current_step = 0
+		self.current_views = []
+		self.candidate_cost = 100
+		self._obs_space = np.zeros(N_JOIN_COMBINATIONS)
+		self._current_action = np.zeros(N_JOIN_COMBINATIONS)
+
 		return self._next_observation()
 
 	def render(self, mode='human', close=False):
 		pass
 
-	def _next_observation(self, params):
-		pass
+	def _next_observation(self):
+		return self._obs_space
 
-	def _take_action(self, action):
-		print('Taking action in environment')
-		
-		if self.current_step < self.max_steps:
-			reward = 1
+	def get_cost_of_episode(self):
+		return random.randint(1, 100)
+
+	def calculate_reward_for_episode(self, cost):
+		return cost
+
+	def _take_action(self, action, candidate, delay_modifier):
+		if action:
+			# Add the created view to the obs space
+			self._obs_space = np.add(self._obs_space, candidate)
+			# Calculate reward
+			if self.current_step < self.max_steps - 1:
+				reward = 1
+			else:
+				# - Do some magic to get cost of the queries
+				# to calculate a useful cost for episode
+				# - Calculate reward using that
+				cost = self.get_cost_of_episode()
+				reward = self.calculate_reward_for_episode(cost)
 		else:
-			# - Do some magic to get cost of the queries
-			# to calculate a useful cost for episode
-			# - Calculate reward using that
-			cost = self.get_cost_of_episode()
-			reward = self.calculate_reward_for_episode()
-		print('Step in environment has completed!')
+			# Add the created view to the obs space
+			# Calculate reward
+			if self.current_step < self.max_steps - 1:
+				reward = 0
+			else:
+				# - Do some magic to get cost of the queries
+				# to calculate a useful cost for episode
+				# - Calculate reward using that
+				cost = self.get_cost_of_episode()
+				reward = self.calculate_reward_for_episode(cost)
 		return reward
 
 # Random testing stuff
-d = DatabaseEnv({'max_steps': 100, 'n_tables': 20})
-print(d)
+env = DatabaseEnv({'max_steps': 20, 'n_tables': 20})
+print(env)
 print("Action Space -")
-print(d.action_space)
+print(env.action_space)
+print(env.action_space.sample())
 print("Observation Space -")
 print("Max values for observation space -")
-print(d.observation_space.high)
+print(env.observation_space.high)
 print("Min values for observation space -")
-print(d.observation_space.low)
+print(env.observation_space.low)
+
+while(1):
+	sampled_action = env.action_space.sample()
+	temp = np.eye(N_JOIN_COMBINATIONS)
+	dummy_candidate = temp[np.random.choice(temp.shape[0], size=1)].squeeze()
+	print('-----------------------------------------------')
+	print('Step - ' + str(env.current_step + 1))
+	print('Action - ' + str(sampled_action))
+	
+	obs = env._next_observation()
+	while np.sum(dummy_candidate * obs) >= 1.0:
+		print(obs * dummy_candidate)
+		dummy_candidate = temp[np.random.choice(temp.shape[0], size=1)].squeeze()
+		# TODO - update LRU information if the query has appeared before	
+
+	obs, reward, done, info = env.step(sampled_action, dummy_candidate)
+	obs = env._next_observation()
+	print('Done - ' + str(done))
+	print('Candidate -')
+	print(dummy_candidate)
+	print('Observation Space -')
+	print(obs)
+	print('Reward - ' + str(reward))
+	if done:
+		print('End of episode!')
+		print('-----------------------------------------------')
+		env.reset()

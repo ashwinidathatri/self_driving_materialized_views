e6889a8eefebe8aa1474c46fdc8cc464ebb3424e
Richhiey Thomas <richhiey.thomas@gmail.com>
2019-07-04 16:01:08 +0200

    Some more changes for EnvRunner

--- project/env/env_runner.py
+++ project/env/env_runner.py
@@ -16,13 +16,13 @@ class EnvRunner():
 	#
 	# 1. Select a query according to the defined workload distribution
 	# 2. Get all candidates for the query and enqueue them
-	# 3. Send all these candidates to the env and let the agent learn
+	# 3. Send all these candidates to the agent and take step in env
 	# 4. Do max_step number of times
 	# 5. Once i == max_step (Episode end) - Replay history of env for episode
 	#
 	###############################################################
-	def run(self):
-		i = 0
+	def run(self, env):
+		step = 0
 
 		queries = []
 		exclusion_list = ['schema.sql', 'fkindexes.sql']
@@ -38,9 +38,9 @@ class EnvRunner():
 		with open(pickle_file_path, 'rb') as pickle_file:
 			candidates = pickle.load(pickle_file)
 
-		while(i < self.max_steps):
-			i = i + 1			
-			# Step 1 - Define distribution and pick query
+		while(step < self.max_steps):
+			step = step + 1			
+			############## Step 1 - Define distribution and pick query ############
 			# TODO - replace with actual database workload distribution
 			
 			# An array of the index value for weighting
@@ -54,7 +54,31 @@ class EnvRunner():
 			selected_query = np.random.choice(queries, size = 1, p = w)[0]
 			print('Current Query - ' + selected_query + ' ...')
 
-			
+			# Hack for parsing processed candidates.
+			# TODO - Fix preprocessing for candidates to return a dictionary
+			# with keys as query names, and list of candidates as values 
+			new_candidates = {}
+			for candidate in candidates:
+				for key, value in candidate.items():
+					new_candidates[key] = value
+
+			print('Current candidate ...')
+			curr_candidates = new_candidates['data/JOB/' + selected_query]
+			print(curr_candidates)
+			###########################################################################
+
+			############## Step 2 - Get all candidates and enqueue ####################
+			for candidate in curr_candidates:
+				candidate = candidate.flatten()
+				# Send to agent and get back action
+				# Currently hacked. Replace with real agent later
+				action = env.action_space.sample()
+				obs, reward, done, info = env.step(action, candidate)
+			###########################################################################
+
+		env.replay_history_on_db()
+		env.reset()
+
 
 params = {
 	'max_steps': 40
